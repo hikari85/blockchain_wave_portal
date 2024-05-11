98d8f39f9b24bcba86289286003e4d8d122edcef
Prabhas Bhat <prabhasbhat2@gmail.com>
2024-05-11 16:47:45 +0530

    wave_portal_project

---
+++ my-wave-portal/contracts/waveportal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a smart contract and I am smart");
+    }
+}
